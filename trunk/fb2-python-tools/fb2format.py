91ff4aa802e8da3083b4f880aff4fa295751a1bf
storchaka <storchaka@2a289c29-9f22-0410-bf74-9e4fdc287b04>
2007-06-14 22:36:14 +0000

    Add option -f for formatting (off by default). Optimize conversion.

--- trunk/fb2-python-tools/fb2format.py
+++ trunk/fb2-python-tools/fb2format.py
@@ -8,6 +8,7 @@ Options:
      -h              display this help message and exit
      -V              display the version and exit
      -e <encoding>   output in the given encoding
+     -f              convert file to human readable format
      -s              sqeeze file in one line
      -b              sqeeze binaries in one line
      -k              create backup files
@@ -29,12 +30,6 @@ import re, base64
 import sys, getopt, os, os.path, xml.dom.minidom, codecs, cStringIO
 
 _spaces_re = re.compile( r'[ \t\r\n]{2,}|[\t\r\n]' )
-_eols_re = re.compile( r'\n\n|\n \n|\n | \n' )
-_end_tag_space_re = re.compile( ' (?=>)| (?=/>)' )
-_start_tag_re = re.compile( '(<[^/?][^>]*>)', re.DOTALL )
-_end_tag_re = re.compile( '(</[^>]*>|<[^/?][^>]*/>)', re.DOTALL )
-def make_tags_switch( tags ):
-	return re.compile( '(%s)' % '|'.join( '<%s(?: [^>]*)?>.*?</%s>' % (tag, tag) for tag in tags ), re.DOTALL )
 def _make_tags_switch( tags ):
 	return re.compile( '(%s)' % '|'.join( '<%s(?: [^>]*)?>.*?</%s>' % (tag, tag) for tag in tags ), re.DOTALL )
 _text_re = _make_tags_switch( ('p', 'v', 'subtitle', 'text-author') )
@@ -50,37 +45,46 @@ def _binary_squeeze( m ):
 def _binary_recode( m ):
 	return m.group( 1 ) + base64.encodestring( base64.decodestring( m.group( 2 ) ) ) + m.group( 3 )
 
+def _squeeze_tag( s ):
+	if _text_re.match( s ):
+		return s
+	else:
+		return s.strip( ' ' ).replace( '> ', '>' ).replace( ' <', '<' )
+
+def _format_tag( s ):
+	if _text_re.match( s ):
+		return s
+	else:
+		return s.strip( ' ' ).replace( '> ', '>' ).replace( ' <', '<' ).replace( '><', '>\n<' )
+
 def fb2format( data, squeeze = False, squeezeBinary = False ):
 	data = _spaces_re.sub( ' ', data )
-	data = _end_tag_space_re.sub( '', data )
 
-	data = _start_tag_re.sub( r'\n\1', data )
-	data = _end_tag_re.sub( r'\1\n', data )
-	data = _text_re.sub( _remove_eols, data )
-	data = _eols_re.sub( '\n', data ).strip()
+	if squeeze:
+		data = ''.join( _squeeze_tag( s ) for s in _text_re.split( data ) )
+		data = data.replace( '>', '>\n', 1 )
+	else:
+		data = '\n'.join( s for s in (_format_tag( s ) for s in _text_re.split( data )) if s )
+		data = _oneline_re.sub( _remove_eols, data )
+		data = data.replace( '>\n<title>', '><title>' )
 
-	data = _oneline_re.sub( _remove_eols, data )
-	if squeezeBinary:
+	if squeezeBinary or squeeze:
 		data = _binary_re.sub( _binary_squeeze, data )
 	else:
 		data = _binary_re.sub( _binary_recode, data )
-
-	if squeeze:
-		data = data.replace( '\n', '' )
-	else:
-		data = data.replace( '>\n<title>', '><title>' )
 
 	return data
 
 
 if __name__ == '__main__':
 	try:
-		opts, args = getopt.getopt( sys.argv[1:], '@:e:hkstvV' )
+		opts, args = getopt.getopt( sys.argv[1:], '@:e:fhkstvV' )
 	except getopt.GetoptError, err:
 		print >>sys.stderr, 'Error:', err
 		sys.exit( 2 )
 
 	forceEncoding = None
+	format = False
 	squeeze = False
 	squeezeBinary = False
 	keepBackup = False
@@ -103,7 +107,10 @@ if __name__ == '__main__':
 			forceEncoding = value
 		elif option == '-k':
 			keepBackup = True
+		elif option == '-f':
+			format = True
 		elif option == '-s':
+			format = True
 			squeeze = True
 		elif option == '-b':
 			squeezeBinary = True
@@ -123,27 +130,24 @@ if __name__ == '__main__':
 
 			doc = xml.dom.minidom.parseString( data0 )
 			encoding = forceEncoding or doc.encoding or 'UTF-8'
-			data = doc.toxml( 'UTF-8' )
-			data = fb2format( data, squeeze = squeeze, squeezeBinary = squeezeBinary )
-			doc = xml.dom.minidom.parseString( data )
+			writer = cStringIO.StringIO()
+			writer = codecs.getwriter( encoding )( writer,  'xmlcharrefreplace' )
+			doc.writexml( writer, encoding = encoding )
+			data = writer.getvalue()
+			writer.close()
+
+			if format:
+				data = fb2format( data, squeeze = squeeze, squeezeBinary = squeezeBinary )
 
 			if filename == '-':
-				writer = codecs.getwriter( encoding )( sys.stdout,  'xmlcharrefreplace' )
-				doc.writexml( writer, encoding = encoding )
-				writer.close()
-			else:
-				writer = cStringIO.StringIO()
-				writer = codecs.getwriter( encoding )( writer,  'xmlcharrefreplace' )
-				doc.writexml( writer, encoding = encoding )
-				data = writer.getvalue()
-				writer.close()
-				if data != data0:
+				sys.stdout.write( data )
+			elif data != data0:
 					open( filename + '.tmp', 'w' ).write( data )
 					if keepBackup:
 						os.rename( filename, filename + backupSuffix )
 					os.rename( filename + '.tmp', filename )
 		except (KeyboardInterrupt, SystemExit):
 			raise
-		except Exception, err:
-			print >>sys.stderr, 'Error processing %s:' % filename
-			print >>sys.stderr, err
+# 		except Exception, err:
+# 			print >>sys.stderr, 'Error processing %s:' % filename
+# 			print >>sys.stderr, err
